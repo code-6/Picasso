9f2a0cfdc9c0272133ef7149630e774fe9cb80aa
wongs <stun@nurtelecom.kg>
2022-10-02 12:32:07 +0600

    refactor tourBind page

--- src/main/java/org/novinomad/picasso/configurations/WebConfig.java
+++ src/main/java/org/novinomad/picasso/configurations/WebConfig.java
@@ -1,35 +1,47 @@
-package org.novinomad.picasso.configurations;
-
-import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    public LayoutDialect layoutDialect() {
-        return new LayoutDialect();
-    }
-//    @Bean
-//    public LocaleResolver localeResolver() {
-//        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-//        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+//package org.novinomad.picasso.configurations;
 //
-//        return sessionLocaleResolver;
-//    }
+//import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
-//    @Bean
-//    public LocaleChangeInterceptor localeChangeInterceptor() {
-//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-//        localeChangeInterceptor.setParamName("lang");
-//        return localeChangeInterceptor;
-//    }
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
 //
 //    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        WebMvcConfigurer.super.addInterceptors(registry);
-//        registry.addInterceptor(localeChangeInterceptor());
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+////        WebMvcConfigurer.super.addResourceHandlers(registry);
+//
+//        registry
+//                .addResourceHandler("/webjars/**")
+//                .addResourceLocations("/webjars/");
 //    }
-}
+//
+//    @Bean
+//    public LayoutDialect layoutDialect() {
+//        return new LayoutDialect();
+//    }
+////    @Bean
+////    public LocaleResolver localeResolver() {
+////        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+////        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+////
+////        return sessionLocaleResolver;
+////    }
+////
+////    @Bean
+////    public LocaleChangeInterceptor localeChangeInterceptor() {
+////        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+////        localeChangeInterceptor.setParamName("lang");
+////        return localeChangeInterceptor;
+////    }
+////
+////    @Override
+////    public void addInterceptors(InterceptorRegistry registry) {
+////        WebMvcConfigurer.super.addInterceptors(registry);
+////        registry.addInterceptor(localeChangeInterceptor());
+////    }
+//}
